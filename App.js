f8361b3f625a86aee65a8e27d40908d798f013a4
ujjwal roy <ujjwal18121999@gmail.com>
2023-03-21 13:19:44 +0530

    react-1

---
+++ App.js
@@ -0,0 +1,14 @@
+// let ele= React.createElement('h2', 
+// {id:'ele'},
+// "Namaste react!");
+let parent= React.createElement('div',
+{id:'parent'},
+[React.createElement('div',{id:'child'},
+[React.createElement('h1',{},'I am h1 tag'),React.createElement('h2',{},'I am h2 tag')
+   ]),
+   React.createElement('div',{id:'child2'},
+   [React.createElement('h1',{},'I am h1 tag'),React.createElement('h2',{},'I am h2 tag')
+      ])]
+);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);  
